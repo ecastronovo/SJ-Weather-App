22f4003efa78cbc65f70ea16522ac0fd7f6cb222
Eric Castronovo <castronovo.eric@gmail.com>
2024-07-31 14:44:53 -0700

    exstract subview button

--- SJ-Weather-App/ContentView.swift
+++ SJ-Weather-App/ContentView.swift
@@ -15,18 +15,8 @@ struct ContentView: View {
             VStack {
                 CityNameView(cityName: "San Jose")
                 
-                VStack(spacing: 8) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40)
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                
                 HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 76)
                     WeatherDayView(dayOfWeek: "WED", imageName: "sun.rain.fill", temperature: 60)
@@ -37,15 +27,7 @@ struct ContentView: View {
                 }
                 Spacer()
                 
-                Button {
-                    print("tapped")
-                } label: {
-                    Text("Change Day Time")
-                        .frame(width: 280, height: 50)
-                        .background(Color.white)
-                        .font(.system(size: 20, weight: .bold, design: .default))
-                        .cornerRadius(10)
-                }
+                WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
 
                 Spacer()
             }
@@ -99,5 +81,44 @@ struct CityNameView: View {
             .font(.system(size: 32, weight: .medium, design: .default))
             .foregroundColor(.white)
             .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            Text(title)
+                .frame(width: 280, height: 50)
+                .background(backgroundColor)
+                .foregroundColor(textColor)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .cornerRadius(10)
+        }
     }
 }
