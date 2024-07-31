0118b696e9e79d88c5dea093488509247a43c204
Eric Castronovo <castronovo.eric@gmail.com>
2024-07-31 12:39:34 -0700

    Initial Commit

---
+++ SJ-Weather-App/SJ_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJ_Weather_AppApp.swift
+//  SJ-Weather-App
+//
+//  Created by Eric Castronovo on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SJ_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
